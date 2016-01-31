65a580b2bafd8fa5f453a9fb57117c3619f61f4a
Anna Rankin <anna@annarankin.com>
2016-01-31 18:37:32 -0500

    modifies config

--- db/config.rb
+++ db/config.rb
@@ -1,9 +1,24 @@
 require "active_record"
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => 'sqlite3',
-  :database => 'db/database.sqlite3'
-)
+if ENV["RACK_ENV"] == "production"
+  # Set up for running postgres instead of sqlite3 on heroku
+    require 'uri'
+    db = URI.parse(ENV['DATABASE_URL'])
+    ActiveRecord::Base.establish_connection({
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    })
+else
+  # This only happens on our local computer!
+  ActiveRecord::Base.establish_connection(
+    :adapter  => 'sqlite3',
+    :database => 'db/database.sqlite3'
+  )
+end
 
 Dir.glob("models/*.rb").each do |path|
   require_relative "../#{path}"
